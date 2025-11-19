a38fb4dee8a82472fd0906d084ec9c88d8da7cec
SuprajaVempalli <supraja.vempalli@ibm.com>
2025-11-19 16:46:10 -0500

    Create rds-mysql module to configure gdp va (#14)
    Signed-off-by: Supraja Vempalli <suprajavempalli@ibm.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Devan Shah <devans@ca.ibm.com>
    Co-authored-by: Supraja Vempalli <suprajavempalli@ibm.com>
    Signed-off-by: Reza Rasolroveicy <roveicy@ibm.com>

---
+++ modules/aws-rds-mysql/outputs.tf
@@ -0,0 +1,17 @@
+#
+# Copyright IBM Corp. 2025
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# RDS MySQL VA Config Module Outputs
+
+output "sqlguard_username" {
+  description = "Username for the Guardium user"
+  value       = var.sqlguard_username
+}
+
+output "sqlguard_password" {
+  description = "Password for the sqlguard user"
+  value       = var.sqlguard_password
+  sensitive   = true
+}
