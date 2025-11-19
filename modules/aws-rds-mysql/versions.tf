fa00f92ffb9a3b1177e256687fab626a457efb66
SuprajaVempalli <supraja.vempalli@ibm.com>
2025-11-19 16:46:10 -0500

    Create rds-mysql module to configure gdp va (#14)
    Signed-off-by: SuprajaVempalli <supraja.vempalli@ibm.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Devan Shah <devans@ca.ibm.com>
    Co-authored-by: SuprajaVempalli <supraja.vempalli@ibm.com>

---
+++ modules/aws-rds-mysql/versions.tf
@@ -0,0 +1,30 @@
+#
+# Copyright IBM Corp. 2025
+# SPDX-License-Identifier: Apache-2.0
+#
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    gdp-middleware-helper  = {
+      source = "IBM/gdp-middleware-helper"
+      version = ">=1.0.0"
+    }
+    guardium-data-protection = {
+      source  = "IBM/guardium-data-protection"
+      version = ">= 1.0.0"
+    }
+  }
+}
