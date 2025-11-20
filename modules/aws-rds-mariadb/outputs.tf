d791ca9a2c013f6e7be4c4d47735f95043703e3d
Reza Rasolroveicy <roveicy@ibm.com>
2025-11-20 16:34:35 -0500

    Add sqlguard user configuration for MariaDB (#18)
    Signed-off-by: Reza Rasolroveicy <roveicy@ibm.com>

--- modules/aws-rds-mariadb/outputs.tf
+++ modules/aws-rds-mariadb/outputs.tf
@@ -1,12 +1,12 @@
-# RDS PostgreSQL VA Config Module Outputs
+# RDS MariaDB VA Config Module Outputs
 
 output "sqlguard_username" {
-  description = "Username for the Guardium user"
-  value       = local.gdmmonitor_username
+  description = "Username for the Guardium VA user"
+  value       = var.sqlguard_username
 }
 
 output "sqlguard_password" {
   description = "Password for the sqlguard user"
-  value       = var.gdmmonitor_password
+  value       = var.sqlguard_password
   sensitive   = true
 }
