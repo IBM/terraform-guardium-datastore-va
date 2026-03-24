47d0f63afec23004e24af337d710a2fabfc9fa8f
Reza Rasolroveicy <roveicy@ibm.com>
2026-03-24 14:54:35 -0400

    fix: Update Azure MySQL datasource template to use correct API format
    - Changed datasource type from 'MySQL' to 'Azure MySQL'
    - Updated template to use lowercase camelCase parameters (name, type, host, port, etc.)
    - Removed Azure credential parameters (clientId, clientSecret, connectionProperty) as they need to be configured separately in Guardium
    - Simplified template variable mapping in main.tf
    - Tested with Guardium REST API and Terraform - datasource registration now works correctly
    - Datasource successfully registered with ID 20014

--- examples/azure-mysql/main.tf
+++ examples/azure-mysql/main.tf
@@ -52,24 +52,13 @@ module "azure_mysql_va_config" {
 
 locals {
   azure_mysql_config = templatefile("${path.module}/templates/azureMysqlVaConf.tpl", {
-    datasource_name             = var.datasource_name
-    datasource_hostname         = var.db_host
-    datasource_port             = var.db_port
-    application                 = var.application
-    datasource_description      = var.datasource_description
-    sqlguard_username           = var.sqlguard_username
-    sqlguard_password           = var.sqlguard_password
-    severity_level              = var.severity_level
-    use_ssl                     = var.use_ssl
-    import_server_ssl_cert      = var.import_server_ssl_cert
-    use_external_password       = var.use_external_password
-    external_password_type_name = var.external_password_type_name
-    azure_key_vault_config_name = var.azure_key_vault_config_name
-    key_vault_name              = var.key_vault_name
-    secret_name                 = var.secret_name
-    client_id                   = var.azure_client_id
-    tenant_id                   = var.azure_tenant_id
-    client_secret               = var.azure_client_secret
+    datasource_name = var.datasource_name
+    host            = var.db_host
+    port            = var.db_port
+    description     = var.datasource_description
+    severity        = var.severity_level
+    db_user         = var.db_username
+    db_password     = var.db_password
   })
   azure_mysql_config_json_decoded = jsondecode(local.azure_mysql_config)
   azure_mysql_config_json_encoded = jsonencode(local.azure_mysql_config)
--- examples/azure-mysql/templates/azureMysqlVaConf.tpl
+++ examples/azure-mysql/templates/azureMysqlVaConf.tpl
@@ -1,29 +1,16 @@
 {
   "name": "${datasource_name}",
-  "type": "AZURE MYSQL",
-  "host": "${datasource_hostname}",
-  "port": ${datasource_port},
-  "application": "${application}",
-  "description": "${datasource_description}",
-  "severity": "${severity_level}",
-  "shared": "Not Shared",
-  "connectionProperties": "{\"clientId\":\"${client_id}\",\"tenantId\":\"${tenant_id}\",\"clientSecret\":\"${client_secret}\"}",
-%{if use_ssl }
-  "importServerSSLcert": ${import_server_ssl_cert ? 1 : 0},
-  "useSSL": 1,
-%{ else }
-  "useSSL": 0,
-%{ endif }
-%{ if use_external_password }
-  "useExternalPassword": 1,
-  "externalPasswordTypeName": "${external_password_type_name}",
-  "azureKeyVaultConfigName": "${azure_key_vault_config_name}",
-  "keyVaultName": "${key_vault_name}",
-  "secretName": "${secret_name}"
-%{ else }
+  "type": "Azure MySQL",
+  "host": "${host}",
+  "port": ${port},
+  "application": "Security Assessment",
+  "description": "${description}",
+  "severity": "${severity}",
   "savePassword": 1,
-  "useExternalPassword": 0,
-  "user": "${sqlguard_username}",
-  "password": "${sqlguard_password}"
-%{ endif }
+  "useSSL": 0,
+  "importServerSSLcert": 0,
+  "useKerberos": 0,
+  "useLDAP": 0,
+  "user": "${db_user}",
+  "password": "${db_password}"
 }
